8584c31c27a6fe8967c1362ca129aadccacbbd0f
hebazo <48584444+hebazo@users.noreply.github.com>
2020-09-20 19:10:47 +0200

    blade and controllers are made

--- database/migrations/2020_09_20_163157_create_products_table.php
+++ database/migrations/2020_09_20_163157_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('beschrijving');
+            $table->string('detail');
             $table->integer('prijs');
             $table->timestamps();
         });
